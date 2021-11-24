0849fa7ba75410fe916cac47917338f66c4c487c
Dhruv Saxena <saxenadhruv1927@gmail.com>
2021-11-24 10:17:00 +0000

    feat(eslint): added linting to the project.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ msg: "Hello World" });
+app.get('/', (req, res) => {
+  res.send({ msg: 'Hello World' });
 });
 
 app.listen(3333, () => {
-  console.log("Server started on port 3333");
+  // eslint-disable-next-line no-console
+  console.log('Server started on port 3333');
 });
